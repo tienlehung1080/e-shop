f40d3dad77add687d8a0a5b35af22c6985de1c03
Tien <tienlehung1080@gmail.com>
2023-09-26 20:39:56 +0700

    Finish

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,23 @@
+import express from "express";
+const router = express.Router();
+import {
+  createProduct,
+  getProduct,
+  getProductById,
+  updateProduct,
+  deleteProduct,
+  createProductReview,
+  getTopProducts,
+} from "../controllers/productController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
+
+router.route("/").get(getProduct).post(protect, admin, createProduct);
+router.get("/top", getTopProducts);
+router
+  .route("/:id")
+  .get(getProductById)
+  .put(protect, admin, updateProduct)
+  .delete(protect, admin, deleteProduct);
+router.route("/:id/reviews").post(protect, createProductReview);
+
+export default router;
